28d94f068ca5fe8c1b6dd3b0ae5db913dad1d50c
Avi Huja <avi.huja@jolt.io>
2020-11-28 16:02:37 +0200

    added puppeteer & simple implementation of handling a scraping task

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,9 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import * as normalizeUrl from 'normalize-url';
+import * as puppeteer from 'puppeteer';
+import { Cluster } from 'puppeteer-cluster';
+import { Movie } from './types';
+
 const IMDB_MOVIES_SHEET_ID = '1--FMdHdg_fnfBvebZwngB1pukZCrlPO5frcVazuP58Q';
 
 @Injectable()
@@ -15,5 +19,33 @@ export class AppService {
       .then((res) =>
         res.data.split('\r\n').map((url: string) => normalizeUrl(url)),
       );
+  }
+
+  async executeTasks(
+    task: (page: puppeteer.Page, url: string) => void,
+    args: string[],
+  ) {
+    const cluster = await Cluster.launch({
+      concurrency: Cluster.CONCURRENCY_CONTEXT,
+      maxConcurrency: 15,
+    });
+
+    await cluster.task(async ({ page, data: url }) => task(page, url));
+
+    await cluster.idle();
+
+    const results = await Promise.all(args.map((a) => cluster.execute(a)));
+
+    await cluster.close();
+
+    return results;
+  }
+
+  async getMovies(urls: string[]) {
+    return this.executeTasks(this.getMovie, urls);
+  }
+
+  async getMovie(page: puppeteer.Page, url: string): Promise<Movie | null> {
+    return null;
   }
 }
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Movie = {
+  name: string;
+  year: string;
+  rating: number;
+  runtime: number;
+  genres: string[];
+  releaseDate: number;
+  budget: number;
+  cumulativeGross: number;
+};
