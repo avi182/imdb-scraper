897f88e92ae08467a08a6d95cd760f58c03b667c
Avi Huja <avi.huja@jolt.io>
2020-11-28 15:50:04 +0200

    Created a new NestJS project and implemted a function that fetch IMDB links from a Google spreadsheet

---
+++ src/app.service.ts
@@ -0,0 +1,19 @@
+import { HttpService, Injectable } from '@nestjs/common';
+import * as normalizeUrl from 'normalize-url';
+const IMDB_MOVIES_SHEET_ID = '1--FMdHdg_fnfBvebZwngB1pukZCrlPO5frcVazuP58Q';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getMoviesUrls() {
+    return this.httpService
+      .get(
+        `https://docs.google.com/spreadsheets/d/${IMDB_MOVIES_SHEET_ID}/export?format=csv`,
+      )
+      .toPromise()
+      .then((res) =>
+        res.data.split('\r\n').map((url: string) => normalizeUrl(url)),
+      );
+  }
+}
